ffbc281a4c3c80ee397e7d65654a30e984499733
Joao Pedro <jpcsnoberto@gmail.com>
2024-01-29 23:05:13 -0300

    feat: new dashboard tables initial design

--- src/app/(private)/(admin)/painel/page.tsx
+++ src/app/(private)/(admin)/painel/page.tsx
@@ -4,8 +4,94 @@ import {AiOutlineCopy, AiOutlineUser, AiOutlineAudit} from 'react-icons/ai'
 
 export default function Hub(){
     return(
-        <section className={styles.container}>
-            
-        </section>
+        <main className={styles.container}>
+            <section className={styles.sectionContainer}>
+
+                <div className={styles.tableContainer}>
+                    <section className={styles.tableHeader}>
+                        <h1>PROCESSOS PERTO DO PRAZO</h1>
+                    </section>
+                    <section className={styles.tableBody}>
+                        <table className={styles.table}>
+                            <thead>
+                                <tr>
+                                    <th className={styles.th}>PROCESSO</th>
+                                    <th className={styles.th}>MATÉRIA</th>
+                                    <th className={styles.th}>CLIENTE</th>
+                                    <th className={styles.th}>ADVOGADO</th>
+                                    <th className={styles.th}>TIPO</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                <tr>
+                                    <td className={styles.td}>TN-238347</td>
+                                    <td className={styles.td}>Aditivo</td>
+                                    <td className={styles.td}>José Bezerra</td>
+                                    <td className={styles.td}>Luiz Rosário</td>
+                                    <td className={styles.td}>Penal</td>
+                                </tr>
+                                <tr>
+                                    <td className={styles.td}>TN-238347</td>
+                                    <td className={styles.td}>Aditivo</td>
+                                    <td className={styles.td}>José Bezerra</td>
+                                    <td className={styles.td}>Luiz Rosário</td>
+                                    <td className={styles.td}>Penal</td>
+                                </tr>
+                                <tr>
+                                    <td className={styles.td}>TN-238347</td>
+                                    <td className={styles.td}>Aditivo</td>
+                                    <td className={styles.td}>José Bezerra</td>
+                                    <td className={styles.td}>Luiz Rosário</td>
+                                    <td className={styles.td}>Penal</td>
+                                </tr>
+                                <tr>
+                                    <td className={styles.td}>TN-238347</td>
+                                    <td className={styles.td}>Aditivo</td>
+                                    <td className={styles.td}>José Bezerra</td>
+                                    <td className={styles.td}>Luiz Rosário</td>
+                                    <td className={styles.td}>Penal</td>
+                                </tr>
+                                <tr>
+                                    <td className={styles.td}>TN-238347</td>
+                                    <td className={styles.td}>Aditivo</td>
+                                    <td className={styles.td}>José Bezerra</td>
+                                    <td className={styles.td}>Luiz Rosário</td>
+                                    <td className={styles.td}>Penal</td>
+                                </tr>
+                                <tr>
+                                    <td className={styles.td}>TN-238347</td>
+                                    <td className={styles.td}>Aditivo</td>
+                                    <td className={styles.td}>José Bezerra</td>
+                                    <td className={styles.td}>Luiz Rosário</td>
+                                    <td className={styles.td}>Penal</td>
+                                </tr>
+                                <tr>
+                                    <td className={styles.td}>TN-238347</td>
+                                    <td className={styles.td}>Aditivo</td>
+                                    <td className={styles.td}>José Bezerra</td>
+                                    <td className={styles.td}>Luiz Rosário</td>
+                                    <td className={styles.td}>Penal</td>
+                                </tr>
+                                <tr>
+                                    <td className={styles.td}>TN-238347</td>
+                                    <td className={styles.td}>Aditivo</td>
+                                    <td className={styles.td}>José Bezerra</td>
+                                    <td className={styles.td}>Luiz Rosário</td>
+                                    <td className={styles.td}>Penal</td>
+                                </tr>
+                                <tr>
+                                    <td className={styles.td}>TN-238347</td>
+                                    <td className={styles.td}>Aditivo</td>
+                                    <td className={styles.td}>José Bezerra</td>
+                                    <td className={styles.td}>Luiz Rosário</td>
+                                    <td className={styles.td}>Penal</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </section>
+                </div>
+            </section>
+        </main>
     )
 }
